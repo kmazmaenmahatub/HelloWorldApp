5c6899f45003417d8dc1286cd70459773e6eb9f6
kmazmaenmahatub <mahatubkmazmaen@gmail.com>
2025-10-01 22:54:00 +0300

    Added second button for text color change

--- app/src/main/java/com/kmazmaenmahatub/helloworldapp/MainActivity.java
+++ app/src/main/java/com/kmazmaenmahatub/helloworldapp/MainActivity.java
@@ -10,19 +10,31 @@ public class MainActivity extends AppCompatActivity {
 
     private TextView textView;
     private Button button;
+    private Button button2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
         textView = findViewById(R.id.textView);
         button = findViewById(R.id.button);
+        button2 = findViewById(R.id.button2); // This was missing!
+
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                textView.setText("Text changed successfullyyy!");
+                textView.setText("Text changed successfully!");
+            }
+        });
+
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                textView.setTextColor(getResources().getColor(android.R.color.holo_red_dark));
             }
         });
     }
